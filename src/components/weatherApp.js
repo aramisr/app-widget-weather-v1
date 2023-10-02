1ff0b948e458186d3a5ac770ee38e1084d278e96
Marcelo Aramis Serpas Zelaya <marcelo.serpas@onelinkbpo.com>
2023-10-01 21:37:52 -0600

    Modify component weatherApp

--- src/components/weatherApp.js
+++ src/components/weatherApp.js
@@ -1,3 +1,11 @@
+import { useState } from "react";
+import WeatherForm from "./weatherForm";
+
 export default function WeatherApp(){
-    return <div>Hola</div>
+    const [weather, setWeather] = useState(null);
+    return(
+        <div>
+            <WeatherForm onChangeCity={handleChangeCity} />;
+        </div>
+    )
 }
