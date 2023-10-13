d81c30f03978df0667aa516b0713d4915ab4ffcd
Marcelo Aramis Serpas Zelaya <marcelo.serpas@onelinkbpo.com>
2023-10-12 20:56:54 -0600

    add handleChangeCity()

--- src/components/weatherApp.js
+++ src/components/weatherApp.js
@@ -3,9 +3,29 @@ import WeatherForm from "./weatherForm";
 
 export default function WeatherApp(){
     const [weather, setWeather] = useState(null);
+
+    async function loadInfo(city = 'london'){
+        try{
+            const request = await fetch(
+                `${process.env.REACT_APP_URL}&key=${process.env.REACT_APP_KEY}&q=${city}`
+            );
+
+            let json = request.json();
+
+            console.log(json);
+        }
+        catch(error){}
+    }
+
+    function handleChangeCity(city){
+        setWeather(null);
+        loadInfo(city);
+    }
+
     return(
         <div>
-            <WeatherForm onChangeCity={handleChangeCity} />;
+            <WeatherForm onChangeCity={handleChangeCity} />
+            <div>Info</div>
         </div>
-    )
+    );
 }
